500b411cd057b486857a1cc6ae60cf382baa6f95
kumsal65 <yahya.alatas.mdbf17@iste.edu.tr>
2020-10-29 20:51:52 +0300

    add new annotated class

--- src/com/kumsal/hibernate/demo/CreateDemo.java
+++ src/com/kumsal/hibernate/demo/CreateDemo.java
@@ -4,6 +4,8 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
+import com.kumsal.demo.entity.Instructor;
+import com.kumsal.demo.entity.InstructorDetail;
 import com.kumsal.demo.entity.Student;
 
 public class CreateDemo {
@@ -11,7 +13,8 @@ public class CreateDemo {
 	public static void main(String[] args) {
 		SessionFactory sessionFactory=new Configuration()
 				.configure("hibernate.cfg.xml")
-				.addAnnotatedClass(Student.class)
+				.addAnnotatedClass(Instructor.class)
+				.addAnnotatedClass(InstructorDetail.class)
 				.buildSessionFactory();
 		
 		Session session=sessionFactory.getCurrentSession();
