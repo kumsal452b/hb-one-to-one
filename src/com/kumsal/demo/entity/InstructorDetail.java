884881c1eaf9af73e93968fbc8d6fbe747d09714
kumsal65 <yahya.alatas.mdbf17@iste.edu.tr>
2020-10-29 20:27:41 +0300

    add new annotatıon

---
+++ src/com/kumsal/demo/entity/InstructorDetail.java
@@ -0,0 +1,10 @@
+package com.kumsal.demo.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="instructor_detail")
+public class InstructorDetail {
+		
+}
