dfce76491c0dd24a8731ec4f87692434df9af194
kumsal65 <yahya.alatas.mdbf17@iste.edu.tr>
2020-10-29 20:41:05 +0300

    add to annotatıon

--- src/com/kumsal/demo/entity/Instructor.java
+++ src/com/kumsal/demo/entity/Instructor.java
@@ -1,18 +1,28 @@
 package com.kumsal.demo.entity;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
 @Table(name="instructor")
 public class Instructor {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	@Column(name="id")
 	private int id;
 	
+	@Column(name="last_name")
 	private String lastName;
 	
+	@Column(name="first_name")
 	private String firstName;
 	
+	@Column(name="email")
 	private String email;
 	
 }
