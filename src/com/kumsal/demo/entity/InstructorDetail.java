ca9555bb9665075ef8271efbcbe3f86b89f1b7c2
kumsal65 <yahya.alatas.mdbf17@iste.edu.tr>
2020-10-29 20:34:10 +0300

    add new class in setter and  getter

--- src/com/kumsal/demo/entity/InstructorDetail.java
+++ src/com/kumsal/demo/entity/InstructorDetail.java
@@ -1,10 +1,56 @@
 package com.kumsal.demo.entity;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Table;
+
 
 @Entity
 @Table(name="instructor_detail")
 public class InstructorDetail {
+		@Id
+		@GeneratedValue(strategy=GenerationType.AUTO)
+		@Column(name="id")
+		private int id;
+		
+		@Column(name="youtube_chanel")
+		private String youTube;
+		
+		@Column(name="hobby")
+		private String hobby;
+
+		public InstructorDetail(int id, String youTube, String hobby) {
+			super();
+			this.id = id;
+			this.youTube = youTube;
+			this.hobby = hobby;
+		}
+
+		public int getId() {
+			return id;
+		}
+
+		public void setId(int id) {
+			this.id = id;
+		}
+
+		public String getYouTube() {
+			return youTube;
+		}
+
+		public void setYouTube(String youTube) {
+			this.youTube = youTube;
+		}
+
+		public String getHobby() {
+			return hobby;
+		}
+
+		public void setHobby(String hobby) {
+			this.hobby = hobby;
+		}
 		
 }
