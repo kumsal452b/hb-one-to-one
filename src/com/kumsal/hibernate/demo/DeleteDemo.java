ae0725237d5164d006886782d9eeef6b2c4e846a
kumsal65 <yahya.alatas.mdbf17@iste.edu.tr>
2020-10-31 11:02:25 +0300

    delete old lınea

---
+++ src/com/kumsal/hibernate/demo/DeleteDemo.java
@@ -0,0 +1,36 @@
+package com.kumsal.hibernate.demo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.kumsal.demo.entity.Instructor;
+import com.kumsal.demo.entity.InstructorDetail;
+import com.kumsal.demo.entity.Student;
+
+public class DeleteDemo {
+
+	public static void main(String[] args) {
+		SessionFactory sessionFactory=new Configuration()
+				.configure("hibernate.cfg.xml")
+				.addAnnotatedClass(Instructor.class)
+				.addAnnotatedClass(InstructorDetail.class)
+				.buildSessionFactory();
+		
+		Session session=sessionFactory.getCurrentSession();
+		
+		session.beginTransaction();
+		try {
+			
+			
+			
+			session.getTransaction().commit();
+			session.close();
+			System.out.println("Saved succesfuly");
+		} finally {
+			// TODO: handle finally clause
+		}
+
+	}
+
+}
